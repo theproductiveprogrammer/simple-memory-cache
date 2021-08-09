35c37e4bb32cea8132857dd28fbab0db91a66464
Charles L <charles.lobo@gmail.com>
2021-08-09 15:57:27 +1000

    feat: sample API response

--- src/main/java/simple/memory/cache/Tester.java
+++ src/main/java/simple/memory/cache/Tester.java
@@ -3,12 +3,23 @@ package simple.memory.cache;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.HttpRequest;
+
+import javax.inject.Inject;
 
 @Controller
 public class Tester {
 
+    @Inject
+    @Client
+    HttpClient client;
+
     @Get
     public String index() {
-        return "hello world!";
+        return client.toBlocking()
+                .retrieve(
+                        HttpRequest.GET("https://reqres.in/api/users?page=2")
+                        );
     }
 }
