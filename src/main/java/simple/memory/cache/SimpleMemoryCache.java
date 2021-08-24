8723a0d6642bf67ff19e31363714044f09b07e55
Charles L <charles.lobo@gmail.com>
2021-08-25 09:37:40 +1000

    refactor: add comments

--- src/main/java/simple/memory/cache/SimpleMemoryCache.java
+++ src/main/java/simple/memory/cache/SimpleMemoryCache.java
@@ -6,11 +6,25 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+/*      way/
+ * This is an in-memory cache that uses a LRU strategy for access. Because
+ * it is dependent on {@link java.util.LinkedHashMap LinkedHashMap} we can't
+ * use a sophisticated eviction stragety and so we use a GC for periodic
+ * collection of expired records.
+ */
 public class SimpleMemoryCache<K,V> {
 
     LRUMap<K,CleanableCacheObject> mem;
     int ttl;
 
+    /*      understand/
+     * creates a LRU in-memory cache with the given number
+     * of max entries and the time to live before expiry
+     * of each entry.
+     *      way/
+     * creates an LRU cache based on {@link java.util.LinkedHashMap LinkedHashMap}
+     * and start a garbage collection cycle.
+     */
     public SimpleMemoryCache(int maxEntries, int ttl) {
         mem = new LRUMap(maxEntries);
 
@@ -31,6 +45,9 @@ public class SimpleMemoryCache<K,V> {
         }
     }
 
+    /*      way/
+     * lock the cache an clear out objects whose time has expired
+     */
     void cleanup() {
         long now = System.currentTimeMillis();
         List<K> todelete = new ArrayList();
@@ -46,6 +63,10 @@ public class SimpleMemoryCache<K,V> {
         Thread.yield();
     }
 
+    /*      way/
+     * return the associated value for this key, updating the access
+     * time - returning null if missing
+     */
     public V get(K k) {
         synchronized(mem) {
             CleanableCacheObject c = mem.get(k);
@@ -55,6 +76,9 @@ public class SimpleMemoryCache<K,V> {
         }
     }
 
+    /*      way/
+     * add a new key value pair to the cache
+     */
     public void put(K k, V v) {
         synchronized(mem) {
             CleanableCacheObject c = new CleanableCacheObject(v);
@@ -62,6 +86,13 @@ public class SimpleMemoryCache<K,V> {
         }
     }
 
+    /*      way/
+     * create a new LRU cache by using {@link java.util.LinkedHashMap LinkedHashMap}
+     * with a 0.75f load factor and a 1/4 initial size which are good
+     * defaults to have. Passing `accessorder=true` ensures that the map
+     * keeps ordered by access which is ideal for LRU and we then use a
+     * simple `removeEldestEntry` eviction strategy.
+     */
     private static class LRUMap<K,V> extends LinkedHashMap<K,V> {
         int maxEntries;
         public LRUMap(int maxEntries) {
@@ -73,6 +104,10 @@ public class SimpleMemoryCache<K,V> {
         }
     }
 
+    /*      understand/
+     * wraps the value so we can detect when the object is too old and
+     * clean it using the GC
+     */
     private class CleanableCacheObject {
         long ts;
         V v;
